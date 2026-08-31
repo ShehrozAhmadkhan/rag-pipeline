cb4d1147a528a522fdd3c879a413105210ee7806
Shehroz Ahmad <shehrozahmadrskh1234@gmail.com>
2026-08-31 13:45:28 +0500

    Batch based embedding.

---
+++ embedding.py
@@ -0,0 +1,23 @@
+from load_data import load_pdf_data,load_text_file_data
+from chunking import word_based_chunking
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+def batch_based_embedding(chunks,batches = 100):
+    all_embeddings = []
+    for i in range(0,len(chunks),batches):
+        batch = chunks[i:i+batches]
+        vectors = client.embeddings.create(model="text-embedding-3-small",input=batch,dimensions=1024)
+        for vector in vectors.data:
+            all_embeddings.append(vector)
+
+    return all_embeddings
+
+data = load_pdf_data("medicine.pdf")
+data_chunks = word_based_chunking(100,20,data)
+chunks_embed = batch_based_embedding(data_chunks)
+print(chunks_embed[0])
